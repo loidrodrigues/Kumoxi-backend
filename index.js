54edfa7703b4600387486c85961ef5fa04d88f1b
Loidrodrigues <loidrodrigues5@gmail.com>
2025-07-11 20:26:33 -0300

    feat: criar a rota de cadastrar novo projeto

--- index.js
+++ index.js
@@ -1,30 +1,24 @@
-import express from 'express'
-import cors from 'cors'
-import dotenv from 'dotenv'
-import projectRoutes from './routes/project.routes.js'
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import projectRoutes from "./routes/project.routes.js";
 
-dotenv.config()
+dotenv.config();
 
-const app = express()
-app.use(cors())
-app.use(express.json())
+const app = express();
+app.use(cors());
+app.use(express.json());
 
-
-
-app.get('/api', (req, res) =>{
-    res.json({message: 'API  Kumoxi online'})
-})
+app.get("/api", (req, res) => {
+  res.json({ message: "API  Kumoxi online" });
+});
 
 // rotas dos projetos
-app.use('/api/projects', projectRoutes)
+app.use("/api/projects", projectRoutes);
+app.use("/api/projects", projectRoutes);
 
+const PORT = process.env.PORT || 3000;
 
-
-
-
-const PORT = process.env.PORT || 3000
-
-app.listen(PORT, () =>{
-    console.log('Servidor Rodando na porta', PORT)
-})
-
+app.listen(PORT, () => {
+  console.log("Servidor Rodando na porta", PORT);
+});
--- routes/project.routes.js
+++ routes/project.routes.js
@@ -1,7 +1,7 @@
-import { Router } from "express"
+import { Router } from "express";
 
-const router = Router()
-const projetos = [
+const router = Router();
+const projects = [
   {
     title: "App de Saúde Mental",
     autor: "Loid Padre",
@@ -14,12 +14,17 @@ const projetos = [
     data: "07/07/2025",
     description: "Plataforma que mostra empregos, bolsas e cursos em Angola.",
   },
-]
+];
 
 router.get("/", (req, res) => {
-    res.json(projetos)
-})
+  res.json(projects);
+});
 
+// routa para cadastrar projetos
 
-
-export default router
+router.post("/", (req, res) => {
+  const newProject = req.body;
+  projects.push(newProject);
+  res.status(201).json({ message: "Projeto cadastrado com sucesso" });
+});
+export default router;
